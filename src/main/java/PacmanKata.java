4ce0bd795ab1e6b494c9ca737320831b9d8ed45b
jzhu0601 <accazk1@gmail.com>
2015-12-03 09:10:53 -0500

    WIP

--- src/main/java/PacmanKata.java
+++ src/main/java/PacmanKata.java
@@ -11,11 +11,10 @@ public class PacmanKata {
     static String[][] board;
     static int pacmanBornHeight;
     static int pacmanBornWidth;
-    static String[][] pacmanLocation;
-    final int UP = 0;
-    final int DOWN = 1;
-    final int LEFT = 2;
-    final int RIGHT = 3;
+    static int firstGhostHeight;
+    static int firstGhostWidth;
+    static int secondGhostHeight;
+    static int secondGhostWidth;
 
     public PacmanKata(int height, int width) {
         boardHeight = height;
@@ -23,61 +22,23 @@ public class PacmanKata {
         board = new String[boardHeight][boardWidth];
         pacmanBornHeight = (int) Math.floor(height / 2);
         pacmanBornWidth = (int) Math.floor(width / 2);
-        pacmanLocation = new String[pacmanBornHeight][pacmanBornWidth];
         for (int i = 0; i < boardHeight; i++) {
             for (int j = 0; j < boardWidth; j++) {
                 board[i][j] = "*";
             }
         }
         board[pacmanBornHeight][pacmanBornWidth] = "V";
+        firstGhostHeight = 0;
+        firstGhostWidth = 0;
+        secondGhostHeight = height - 1;
+        secondGhostWidth = width - 1;
+        board[firstGhostHeight][firstGhostWidth] = "@";
+        board[secondGhostHeight][secondGhostWidth] = "@";
+
     }
 
-//    public void tickUp(int steps) {
-//        int counter = 1;
-//        int timer = 0;
-//        for (int loop = 0; loop < steps; loop++) {
-//            board[pacmanBornHeight - counter][pacmanBornWidth] = "V";
-//            for (int innerTimer = 0; innerTimer < counter; innerTimer++) {
-//                board[pacmanBornHeight - innerTimer][pacmanBornWidth] = " ";
-//            }
-//            counter++;
-//            timer++;
-//        }
-//
-//
-//        for (int i = 0; i < boardHeight; i++) {
-//            for (int j = 0; j < boardWidth; j++) {
-//                System.out.print(board[i][j]);
-//            }
-//            System.out.println();
-//        }
-//        System.out.println("-------------------------");
-//    } //end going UP
 
-
-//    public void tickDown(int steps) {
-//        int counter = 1;
-//        int timer = 0;
-//        for (int loop = 0; loop < steps; loop++) {
-//            board[pacmanBornHeight + counter][pacmanBornWidth] = "V";
-//            for (int innerTimer = 0; innerTimer < counter; innerTimer++) {
-//                board[pacmanBornHeight + innerTimer][pacmanBornWidth] = " ";
-//            }
-//            counter++;
-//            timer++;
-//        }
-//
-//        for (int i = 0; i < boardHeight; i++) {
-//            for (int j = 0; j < boardWidth; j++) {
-//                System.out.print(board[i][j]);
-//            }
-//            System.out.println();
-//        }
-//        System.out.println("-------------------------");
-//    }//end tickDown
-
-
-    public void tickUp(int steps) {
+    public int tickUp(int steps) {
         int counter = 1;
         int timer = 0;
         for (int loop = 0; loop < steps; loop++) {
@@ -100,9 +61,10 @@ public class PacmanKata {
             System.out.println();
         }
         System.out.println("-------------------------");
+        return pacmanBornHeight;
     }//end tickDown
 
-    public void tickDown(int steps) {
+    public int tickDown(int steps) {
         int counter = 1;
         int timer = 0;
         for (int loop = 0; loop < steps; loop++) {
@@ -125,17 +87,23 @@ public class PacmanKata {
             System.out.println();
         }
         System.out.println("-------------------------");
+        return pacmanBornHeight;
     }//end tickDown
 
-    public void tickRight(int steps) {
+    public int tickRight(int steps) {
         int counter = 1;
         int timer = 0;
+
         for (int loop = 0; loop < steps; loop++) {
-            board[pacmanBornHeight][pacmanBornWidth + counter] = "V";
             for (int innerTimer = 0; innerTimer < counter; innerTimer++) {
                 board[pacmanBornHeight][pacmanBornWidth + innerTimer] = " ";
             }
-            counter++;
+            pacmanBornWidth += counter;
+            //Allow for wraparounds:
+            if (pacmanBornWidth > board[0].length) {
+                pacmanBornWidth = 0;
+            }
+            board[pacmanBornHeight][pacmanBornWidth] = "V";
             timer++;
         }
 
@@ -146,19 +114,26 @@ public class PacmanKata {
             System.out.println();
         }
         System.out.println("-------------------------");
+        return pacmanBornWidth;
     } //end tickRight
 
-    public void tickLeft(int steps) {
+    public int tickLeft(int steps) {
         int counter = 1;
         int timer = 0;
+
         for (int loop = 0; loop < steps; loop++) {
-            board[pacmanBornHeight][pacmanBornWidth - counter] = "V";
             for (int innerTimer = 0; innerTimer < counter; innerTimer++) {
                 board[pacmanBornHeight][pacmanBornWidth - innerTimer] = " ";
             }
-            counter++;
+            pacmanBornWidth -= counter;
+            //Allow for wraparounds:
+            if (pacmanBornWidth > board[0].length) {
+                pacmanBornWidth = 0;
+            }
+            board[pacmanBornHeight][pacmanBornWidth] = "V";
             timer++;
         }
+
         for (int i = 0; i < boardHeight; i++) {
             for (int j = 0; j < boardWidth; j++) {
                 System.out.print(board[i][j]);
@@ -166,22 +141,28 @@ public class PacmanKata {
             System.out.println();
         }
         System.out.println("-------------------------");
+        return pacmanBornWidth;
     } //end tickLeft
 
+    public void ghostRandomMovement(){
 
-//    public boolean boardCreated() {
-//        return board[0].length > 0 && board.length > 0;
-//    }
+    }
 
-//    public boolean pacmanCreated() {
-//        return pacmanBornHeight >= 0 && pacmanBornWidth >= 0;
-//    }
+    public boolean boardCreated() {
+        return board[0].length > 0 && board.length > 0;
+    }
+
+    public boolean pacmanCreated() {
+        return pacmanBornHeight >= 0 && pacmanBornWidth >= 0;
+    }
 
 
     public static void main(String args[]) {
         PacmanKata pacmanKata = new PacmanKata(10, 10);
         pacmanKata.tickUp(3);
-        pacmanKata.tickDown(3);
-//        pacmanKata.tickLeft(3);
+        pacmanKata.tickDown(7);
+        pacmanKata.tickRight(3);
+        pacmanKata.tickLeft(5);
     }
+
 }
